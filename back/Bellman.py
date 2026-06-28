e9de931769bef915d39ad5787e10eadd1ca605f1
gasimiaro <zagasimiaro@gmail.com>
2026-06-28 04:15:45 +0300

    fix: add before_request handler to explicitly handle CORS preflight

--- back/Bellman.py
+++ back/Bellman.py
@@ -6,6 +6,15 @@ import os
 app = Flask(__name__)
 cors = CORS(app, resources={r"/*": {"origins": "*"}})
 
+@app.before_request
+def handle_preflight():
+    if request.method == 'OPTIONS':
+        response = make_response()
+        response.headers.add('Access-Control-Allow-Origin', '*')
+        response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+        response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+        return response
+
 @app.after_request
 def after_request(response):
   response.headers.add('Access-Control-Allow-Origin', '*')
